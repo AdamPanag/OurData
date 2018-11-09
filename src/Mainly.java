c20aab64a2c6700de9f126e0fa7c4f57d41ccf9a
AdamPanag <adampanag06@gmail.com>
2018-11-09 12:50:18 +0200

    create menu

--- src/Mainly.java
+++ src/Mainly.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
 
 public class Mainly {
 	public static void main(String[] args) {
+		
+		Scanner input = new Scanner (System.in);
 
-		System.out.println("Hello spin ,spin ,spinnn");
-		System.out.println("Hello World");
-		System.out.println("Void was here");
+		System.out.println("** Hello World **" + 
+						   "\n\nThis application creates a data base! Let's begin!\n");
+		
+		int ans;
+		do {
+			
+			System.out.println("** Menu **" 
+							   + "\n\n0. Terminate application"
+							   + "\n1. Create a new data base. "
+							   + "\n2. Add fields in existing data base."
+							   + "\n3. Delete fields."
+							   + "\n4. Edit fields."
+							   + "\n\nWhat do you want to do?");
+			
+			ans = input.nextInt();
+			
+		} while (ans != 0);
+
 
 	}
 }
